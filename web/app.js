fc66cd9dc136252f7aad0bd4a2a89e70ae279c8d
Daniel Whatley <dwhatley@mit.edu>
2020-09-23 09:28:14 -0500

    instructor mode + page updates after stop typing

--- web/app.js
+++ web/app.js
@@ -40,6 +40,7 @@ class App extends React.Component {
       drawerOpen: true,
       label: "Type Snapdown here",
     };
+    this.timeout = 0;
   }
 
   downloadSVG() {
@@ -67,11 +68,12 @@ class App extends React.Component {
   }
 
   redraw(id, newText) {
+    let toRemove = [];
     Object.keys(this.state.created).map((x) => {
       if (x == id) {
         this.state.created[x].map((y) => {
           let element = document.getElementById(y);
-          if (element) element.remove();
+          if (element) toRemove.push(element);
         });
       }
     });
@@ -85,6 +87,7 @@ class App extends React.Component {
     try {
       created[id] = Snapdown.render(scriptElement);
       error[id] = false;
+      for (var x of toRemove) x.remove();
       this.setState({ error: error, created: created });
     } catch (err) {
       error[id] = true;
@@ -128,24 +131,40 @@ class App extends React.Component {
     if (!this.state.currentExample) {
       boxes.snapExample.display = "none";
     }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const instructorMode = urlParams.get("mode") === "instructor";
+
+    let textFieldStyle = { width: instructorMode ? "40%" : "20%" };
+    if (instructorMode) {
+      textFieldStyle["height"] = "100%";
+    }
+
     return (
       <div>
-        <AppBar position="static" style={{ marginBottom: "20px" }}>
-          <Toolbar>
-            <Typography variant="h5" style={{ margin: "10px", flex: 1 }}>
-              Snapdown
-            </Typography>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="end"
-              onClick={() => this.handleDrawerOpen()}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <div style={{ marginLeft: "2%" }}>
+        {!instructorMode && (
+          <AppBar position="static" style={{ marginBottom: "20px" }}>
+            <Toolbar>
+              <Typography variant="h5" style={{ margin: "10px", flex: 1 }}>
+                Snapdown
+              </Typography>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="end"
+                onClick={() => this.handleDrawerOpen()}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        )}
+        <div
+          style={Object.assign(
+            { marginLeft: "2%" },
+            instructorMode ? { marginTop: "2%" } : {}
+          )}
+        >
           {Object.keys(boxes).map((x) => (
             <div
               style={{
@@ -157,99 +176,114 @@ class App extends React.Component {
               <div style={{ display: "flex", flexDirection: "row" }}>
                 <TextField
                   onChange={(event) => {
-                    this.setSnapdownText(x, event.target.value);
+                    let snapdownText = event.target.value;
+                    if (this.timeout) clearTimeout(this.timeout);
+                    this.timeout = setTimeout(() => {
+                      this.setSnapdownText(x, snapdownText);
+                    }, 300);
                     this.setState({ label: null });
                   }}
                   defaultValue={this.state.snapdownText[x]}
                   multiline={true}
                   variant="outlined"
-                  style={{ width: "20%", margin: boxes[x].margin }}
+                  style={Object.assign(textFieldStyle, {
+                    margin: boxes[x].margin,
+                  })}
                   disabled={boxes[x].disabled}
                   InputProps={{ style: { color: "black" } }}
                   label={boxes[x].label}
+                  error={this.state.error[x]}
+                  helperText={
+                    this.state.error[x] ? "Unable to parse Snapdown input." : ""
+                  }
                 />
                 <div style={{ marginLeft: "2%" }}>
                   <script type="application/snapdown" id={x} />
-                  {this.state.error[x] && <div>Error occurred.</div>}
                 </div>
               </div>
               <br />
             </div>
           ))}
-          <Button
-            variant="contained"
-            onClick={() => {
-              navigator.clipboard.writeText(window.location.href);
-            }}
-          >
-            Copy Direct URL
-          </Button>
-          <br />
-          <br />
-          <Button variant="contained" onClick={this.downloadSVG}>
-            Export to SVG
-          </Button>
-        </div>
-        <Drawer
-          variant="persistent"
-          anchor="right"
-          open={this.state.drawerOpen}
-        >
-          <div style={{ width: drawerWidth }}>
+          {!instructorMode && (
             <div>
-              <IconButton onClick={() => this.handleDrawerClose()}>
-                <ChevronRightIcon />
-                <small>hide</small>
-              </IconButton>
-              <Divider />
+              <Button
+                variant="contained"
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                }}
+              >
+                Copy Direct URL
+              </Button>
+              <br />
+              <br />
+              <Button variant="contained" onClick={this.downloadSVG}>
+                Export to SVG
+              </Button>
             </div>
-            <div style={{ margin: "5%" }}>
-              Snapdown is a language for <b>drawing</b> snapshot diagrams. Type
-              in the text box to start drawing diagrams.
-              <br />
-              <br />
-              Click on the links below to see examples of Snapdown in action!
-              <br />
-              <br />
-              {Object.keys(examples).map((x) => (
-                <div>
-                  <a
-                    href="#"
-                    onClick={(e) => {
-                      if (x == this.state.currentExample) {
-                        this.setState({ currentExample: null });
-                      } else {
-                        this.setSnapdownText(
-                          "snapExample",
-                          examples[x].snapdown
-                        );
-                        this.setState({ currentExample: x });
-                      }
-                      e.preventDefault();
-                    }}
-                  >
-                    {examples[x].name}
-                  </a>
-                  {x == this.state.currentExample && (
-                    <font color="gray">&nbsp;(click again to hide)</font>
-                  )}
-                  <br />
-                  <div
-                    style={{
-                      marginLeft: "5%",
-                      display:
-                        x == this.state.currentExample ? "block" : "none",
-                    }}
-                  >
-                    {examples[x].explanation}
+          )}
+        </div>
+        {!instructorMode && (
+          <Drawer
+            variant="persistent"
+            anchor="right"
+            open={this.state.drawerOpen}
+          >
+            <div style={{ width: drawerWidth }}>
+              <div>
+                <IconButton onClick={() => this.handleDrawerClose()}>
+                  <ChevronRightIcon />
+                  <small>hide</small>
+                </IconButton>
+                <Divider />
+              </div>
+              <div style={{ margin: "5%" }}>
+                Snapdown is a language for <b>drawing</b> snapshot diagrams.
+                Type in the text box to start drawing diagrams.
+                <br />
+                <br />
+                Click on the links below to see examples of Snapdown in action!
+                <br />
+                <br />
+                {Object.keys(examples).map((x) => (
+                  <div>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        if (x == this.state.currentExample) {
+                          this.setState({ currentExample: null });
+                        } else {
+                          this.setSnapdownText(
+                            "snapExample",
+                            examples[x].snapdown
+                          );
+                          this.setState({ currentExample: x });
+                        }
+                        e.preventDefault();
+                      }}
+                    >
+                      {examples[x].name}
+                    </a>
+                    {x == this.state.currentExample && (
+                      <font color="gray">&nbsp;(click again to hide)</font>
+                    )}
+                    <br />
+                    <div
+                      style={{
+                        marginLeft: "5%",
+                        display:
+                          x == this.state.currentExample ? "block" : "none",
+                      }}
+                    >
+                      {examples[x].explanation}
+                    </div>
+                    <Divider />
+                    <br />
                   </div>
-                  <Divider />
-                  <br />
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        </Drawer>
+          </Drawer>
+        )}
       </div>
     );
   }
