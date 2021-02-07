a120183674cc2f726eb0efeb11473438e6de818c
Daniel Whatley <dwhatley@mit.edu>
2021-02-06 19:10:19 -0600

    constellation.js

--- constellation.js
+++ constellation.js
@@ -1,2 +1,24 @@
+let diagram = document.createElement('div');
+diagram.id = 'diagram';
 
+let br = document.createElement('br');
+
+let error = document.createElement('font');
+error.id = 'error';
+error.color = 'red';
+
+[diagram, br, error].forEach(x => {
+  document.getElementById('extra').appendChild(x);
+});
+
+doc.on('op', () => {
+  let input = document.getElementById('text').value;
+  try {
+    Snapdown.renderText(input, 'diagram', (combined) => {
+      document.getElementById('diagram').replaceChildren(combined);
+    });
+  } catch (e) {
+    document.getElementById('error').text = e.message;
+  }
+});
 
